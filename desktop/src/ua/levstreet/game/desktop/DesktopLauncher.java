db22e22b93100ce014d3f7005e7fdc8123295d3c
SanekP <sanekne@mail.ru>
2014-09-15 20:59:04 +0300

    Initial commit

---
+++ desktop/src/ua/levstreet/game/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package ua.levstreet.game.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import ua.levstreet.game.MyGdxGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new MyGdxGame(), config);
+	}
+}
