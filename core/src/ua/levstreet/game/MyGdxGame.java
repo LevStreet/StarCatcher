15d3310b7534e1ef93abdb98427aaf6af1b1b2d9
SanekP <sanekne@mail.ru>
2014-09-21 00:58:56 +0300

    Add flow and states

--- core/src/ua/levstreet/game/MyGdxGame.java
+++ core/src/ua/levstreet/game/MyGdxGame.java
@@ -8,18 +8,23 @@ import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.MathUtils;
 import com.badlogic.gdx.math.Vector3;
 
 public class MyGdxGame extends ApplicationAdapter {
-	private OrthographicCamera camera;
-	private SpriteBatch batch;
 	private final int WIDTH = 800;
 	private final int HEIGHT = 480;
+	private final int NUMBER_OF_STARS = 10;
+	private final int FLOW_WIDTH = 100;
+	private final int STAR_WIDTH = 50;
+	private final int STAR_HEIGHT = 50;
+	private OrthographicCamera camera;
+	private SpriteBatch batch;
 	private Vector3 touchPos = new Vector3();
 	private Vector3 grabbedPos = new Vector3();
 	private BitmapFont bitmapFont;
-	private Star star;
-	private boolean grabbed;
+	private Star grabbed;
+	private Star[] stars = new Star[NUMBER_OF_STARS];
 
 	@Override
 	public void create() {
@@ -28,33 +33,50 @@ public class MyGdxGame extends ApplicationAdapter {
 		batch = new SpriteBatch();
 		touchPos.set(WIDTH / 2, HEIGHT / 2, 0);
 		bitmapFont = new BitmapFont();
-		star = new Star(WIDTH / 2, HEIGHT / 2, 50, 50);
-		star.setBoundary(0, 0, WIDTH, HEIGHT);
+		for (int i = 0; i < NUMBER_OF_STARS; i++) {
+			stars[i] = new Star(MathUtils.random(0, FLOW_WIDTH), HEIGHT
+					+ MathUtils.random(0, 500), STAR_WIDTH, STAR_HEIGHT);
+			stars[i].setBoundary(0, 0, WIDTH, HEIGHT);
+			stars[i].setDirection(0, -MathUtils.random(10, 100));
+		}
 	}
 
 	@Override
 	public void render() {
-		Gdx.gl.glClearColor(0, 0, 0.2f, 1);
-		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		if (Gdx.input.isTouched()) {
 			touchPos.set(Gdx.input.getX(), Gdx.input.getY(), 0);
 			camera.unproject(touchPos);
-			if (!grabbed
-					&& star.getRectangle().contains(touchPos.x, touchPos.y)) {
-				grabbed = true;
-				grabbedPos.set(touchPos);
+			for (Star star : stars) {
+				if (grabbed == null
+						&& star.getRectangle().contains(touchPos.x, touchPos.y)) {
+					grabbed = star;
+					grabbedPos.set(touchPos);
+				}
 			}
 		} else {
-			grabbed = false;
+			if (grabbed != null) {
+				grabbed.setState(Star.State.RELEASED);
+				grabbed = null;
+			}
 		}
-		if (grabbed) {
-			star.setDirection(touchPos.x - grabbedPos.x, touchPos.y
+		if (grabbed != null) {
+			grabbed.setDirection(touchPos.x - grabbedPos.x, touchPos.y
 					- grabbedPos.y);
 		}
 		camera.update();
+		Gdx.gl.glClearColor(0, 0, 0.2f, 1);
+		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		batch.setProjectionMatrix(camera.combined);
 		batch.begin();
-		star.draw(batch);
+		for (Star star : stars) {
+			if (star.getState() == Star.State.DEAD) {
+				star.getRectangle().x = MathUtils.random(0, FLOW_WIDTH);
+				star.getRectangle().y = HEIGHT + MathUtils.random(0, 500);
+				star.setDirection(0, -MathUtils.random(10, 100));
+				star.setState(Star.State.IN_FLOW);
+			}
+			star.draw(batch);
+		}
 		String fps = "FPS: " + Gdx.graphics.getFramesPerSecond();
 		bitmapFont.draw(batch, fps, 0, HEIGHT - bitmapFont.getXHeight());
 		batch.end();
--- core/src/ua/levstreet/game/entity/Star.java
+++ core/src/ua/levstreet/game/entity/Star.java
@@ -7,7 +7,13 @@ import com.badlogic.gdx.math.Rectangle;
 import com.badlogic.gdx.math.Vector2;
 
 public class Star {
+	public static enum State {
+		IN_FLOW, RELEASED, DEAD
+	}
+
 	private final int SPEED = 10;
+	private final float DEAD_DELTA = .01f;
+	private State state;
 	private Rectangle rectangle;
 	private Rectangle boundary = new Rectangle();
 	private Vector2 direction = new Vector2();
@@ -15,6 +21,7 @@ public class Star {
 
 	public Star(float x, float y, float width, float height) {
 		rectangle = new Rectangle(x, y, width, height);
+		state = State.IN_FLOW;
 	}
 
 	public Rectangle getRectangle() {
@@ -34,22 +41,41 @@ public class Star {
 	}
 
 	public void draw(SpriteBatch batch) {
-		if (rectangle.x < boundary.x && direction.x < 0
-				|| rectangle.x > boundary.width - rectangle.width
-				&& direction.x > 0) {
-			direction.x *= -1;
+		if (state != State.DEAD) {
+			float deltaTime = Gdx.graphics.getDeltaTime();
+			rectangle.x += direction.x * deltaTime * SPEED;
+			rectangle.y += direction.y * deltaTime * SPEED;
+			if (state == State.RELEASED) {
+				if (rectangle.x < boundary.x && direction.x < 0
+						|| rectangle.x > boundary.width - rectangle.width
+						&& direction.x > 0) {
+					direction.x *= -1;
+				}
+				if (rectangle.y < boundary.y && direction.y < 0
+						|| rectangle.y > boundary.height - rectangle.height
+						&& direction.y > 0) {
+					direction.y *= -1;
+				}
+				direction.x *= 1 - deltaTime;
+				direction.y *= 1 - deltaTime;
+				if (Math.abs(direction.x) < DEAD_DELTA
+						&& Math.abs(direction.y) < DEAD_DELTA) {
+					state = State.DEAD;
+				}
+			}
+			if (state == State.IN_FLOW && rectangle.y < -rectangle.height) {
+				state = State.DEAD;
+			}
+			batch.draw(texture, rectangle.x, rectangle.y, rectangle.width,
+					rectangle.height);
 		}
-		if (rectangle.y < boundary.y && direction.y < 0
-				|| rectangle.y > boundary.height - rectangle.height
-				&& direction.y > 0) {
-			direction.y *= -1;
-		}
-		float deltaTime = Gdx.graphics.getDeltaTime();
-		rectangle.x += direction.x * deltaTime * SPEED;
-		rectangle.y += direction.y * deltaTime * SPEED;
-		direction.x *= 1 - deltaTime;
-		direction.y *= 1 - deltaTime;
-		batch.draw(texture, rectangle.x, rectangle.y, rectangle.width,
-				rectangle.height);
+	}
+
+	public State getState() {
+		return state;
+	}
+
+	public void setState(State state) {
+		this.state = state;
 	}
 }
